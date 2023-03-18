6ee6bdf4286148b58ee0f47684cb975f6356f714
Nemmy <uleluemmanuel@gmail.com>
2023-03-18 16:27:06 +0100

    vue public

---
+++ public/WildCard.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="wildCard">
+        <h1>404</h1>
+        <p>The requested page was not found</p>
+        <img src="/src/Images/404.png" alt="oops!">
+    </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+
+    .wildCard {
+        margin-top: 2rem;
+        padding: 10px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+    
+    h1 {
+        font-family: 'Poppins', sans-serif;
+        color: lightseagreen;
+        /* font-weight: 2000px; */
+        font-size: 70px;
+    }
+
+    p {
+        font-family: 'Poppins', sans-serif;
+        font-weight: 600;
+    }
+</style>
