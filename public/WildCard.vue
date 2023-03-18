dcfeebad073e219d89194e5311010b4049e1fb2c
Nemmy <uleluemmanuel@gmail.com>
2023-03-18 16:37:13 +0100

    home button

--- public/WildCard.vue
+++ public/WildCard.vue
@@ -1,34 +1,57 @@
 <template>
-    <div class="wildCard">
-        <h1>404</h1>
-        <p>The requested page was not found</p>
-        <img src="/src/Images/404.png" alt="oops!">
+  <div class="wildCard">
+    <h1>404</h1>
+    <p>The requested page was not found</p>
+    <img src="/src/assets/logo.png" alt="oops!" />
+    <div class="btn">
+      <RouterLink to="/" class="banner-btn" rel="noreferrer"> Home </RouterLink>
     </div>
+  </div>
 </template>
 
-<script>
-</script>
+<script></script>
 
 <style scoped>
+.wildCard {
+  margin-top: 2rem;
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
 
-    .wildCard {
-        margin-top: 2rem;
-        padding: 10px;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-    }
-    
-    h1 {
-        font-family: 'Poppins', sans-serif;
-        color: lightseagreen;
-        /* font-weight: 2000px; */
-        font-size: 70px;
-    }
+h1 {
+  font-family: "Poppins", sans-serif;
+  color: lightseagreen;
+  /* font-weight: 2000px; */
+  font-size: 70px;
+}
 
-    p {
-        font-family: 'Poppins', sans-serif;
-        font-weight: 600;
-    }
+p {
+  font-family: "Poppins", sans-serif;
+  font-weight: 600;
+}
+
+.banner-btn {
+  padding: 10px 30px;
+  text-transform: uppercase;
+  text-decoration: none;
+  border-radius: 5px;
+  font-family: "Poppins", sans-serif;
+  font-weight: bold;
+  margin-right: 10px;
+  transition: all 0.5s;
+  color: white;
+  background-color: lightseagreen;
+  border: 2px solid lightseagreen;
+}
+
+.banner-btn:hover {
+  color: lightseagreen;
+  background: #fff;
+}
+.btn {
+    margin-top: 15px;
+}
 </style>
